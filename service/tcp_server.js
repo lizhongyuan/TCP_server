0c6cd8c8439990e90611b8f657ee09677e1a77b8
Li Zhongyuan <fenixlee@163.com>
2017-12-08 21:11:37 -0800

    tmp

--- service/tcp_server.js
+++ service/tcp_server.js
@@ -15,11 +15,16 @@ const os = require("os");
 module.exports.createServer = (redis, db, timeout) => {
     return net.createServer((client) => {
 
+        let curImei;
+
         // 服务器收到数据data时的
         client.on("data", data => {
             //
             let imei = packet.imei;
             let packetHandler = LockHandler.getLockHandler(imei);
+            if(!packetHandler) { return; }
+
+            curImei = packetHandler.imei;
 
             client.emit("ack_lock_finish", packetHandler);
         });
@@ -29,32 +34,33 @@ module.exports.createServer = (redis, db, timeout) => {
         client.on("ack_lock_finish", OnLockData)
     })
 
+    // 只进行一次, 建立新的[长连接, 设备(lock), 业务Emitter] <--> imei 映射
+    function buildLongConnection(packet) {
+
+        // 登记设备, 如果有, 则写入_activeLock
+        let imei = packet.imei;
+        client.imei = imei;
+        LockHandler.add(imei);
+
+
+        /* 重新监听外部 CLI 事件 , 当侦听到imei的信号后，由sendMsg2Lock */
+        downStreamEmitter.removeAllListeners(imei);
+        downStreamEmitter.on(imei, sendMsg2Lock);
+    }
 
     function OnLockData(packet) {
         let imei = packet.imei;
         let lockHandler = lockHandler.getH
     }
+
+
+    function sendMsg2Lock(packet) {
+        client.write(packet.toString());
+    }
 }
 
 
 
-// 只进行一次, 建立新的[长连接, 设备(lock), 业务Emitter] <--> imei 映射
-let buildLongConnection = (packet) => {
 
-    // 登记设备, 如果有, 则写入_activeLock
-    let imei = packet.imei;
-    client.imei = imei;
-    LockHandler.add(imei);
 
-    setSaveElecMode(imei)
-        .then(res => {
-            logger.info(res)
-        }, err => {
-            logger.debug(JSON.stringify(err))
-        })
-
-    /* 重新监听外部 CLI 事件 , 当侦听到imei的信号后，由sendMsg2Lock */
-    downStreamEmitter.removeAllListeners(imei);
-    downStreamEmitter.on(imei, sendMsg2Lock);
-}
 
